33d4f7864f84b8255f79535c20d5565e53236c5e
xjakubeco <102169575+xjakubeco@users.noreply.github.com>
2022-03-24 22:48:14 +0100

    Added index.html and others

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let names = ["Martin","Andrej","Petra","Stefan","Oliver"]
+
+names.forEach(element => {
+    
+});
+
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  }
+
+
+
+
+  $("main").append("<h1>Added with javascript</h1>", "<p>This block was added using JavaScript's jQuery library. How awesome!</p>");
